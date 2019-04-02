914b006fb28f796a7168276d5f759332c330d2d3
secsilm <secsilm@outlook.com>
2019-04-03 07:52:20 +0800

    Feat: add code for showing process info and gpu power info Refactor: use namedtuple Device and Process instead of dict

--- utils.py
+++ utils.py
@@ -30,32 +30,87 @@ def get_infos():
     """
 
     infos = {}
-    Device = namedtuple("Device", ["free", "used", "total", "temperature", "fan_speed"])
-    Process = namedtuple("Process", ["pid". "memory_percent", "status", "username", "num_threads", "cpu_num", "cpu_percent", "name", "cmdline", "used_gpu_mem"])
+    Device = namedtuple(
+        "Device",
+        [
+            "free",
+            "used",
+            "total",
+            "temperature",
+            "fan_speed",
+            "power_usage",
+            "power_state",
+            "process",
+        ],
+    )
+    Process = namedtuple(
+        "Process",
+        [
+            "pid",
+            "memory_percent",
+            "status",
+            "username",
+            "num_threads",
+            "cpu_num",
+            "cpu_percent",
+            "name",
+            "cmdline",
+            "used_gpu_mem",
+            "create_time"
+        ],
+    )
     pynvml.nvmlInit()
     driver_version = pynvml.nvmlSystemGetDriverVersion().decode()
     device_count = pynvml.nvmlDeviceGetCount()
-    info = {}
+    info = []
     for i in range(device_count):
-        info[i] = {}
         handle = pynvml.nvmlDeviceGetHandleByIndex(i)
         mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle)
         power_usage = pynvml.nvmlDeviceGetPowerUsage(handle)  # 电源使用量，单位为毫瓦 mW
         processes = pynvml.nvmlDeviceGetComputeRunningProcesses(handle)  # 哪些进程在使用该 GPU
-        process_info = [(item.pid, item.usedGpuMemory) for item in process_info]
+        # process_info = [(item.pid, item.usedGpuMemory) for item in process_info]
         process_info = []
         for p in processes:
             # append Process object to process_info
-            pass
+            pid = p.pid
+            used_gpu_mem = p.usedGpuMemory
+            p = psutil.Process(pid=pid)
+            process_info.append(
+                Process(
+                    pid=pid,
+                    memory_percent=p.memory_percent(),
+                    status=p.status(),
+                    username=p.username(),
+                    num_threads=p.num_threads(),
+                    cpu_num=p.cpu_percent(),
+                    cpu_percent=p.cpu_percent(),
+                    name=p.name(),
+                    cmdline=p.cmdline(),
+                    used_gpu_mem=used_gpu_mem,
+                    create_time=p.create_time()
+                )
+            )
         try:
             fan_speed = pynvml.nvmlDeviceGetFanSpeed(handle)
         except pynvml.NVMLError_NotSupported as e:
             fan_speed = None
-        info[i]['free'] = mem_info.free
-        info[i]['used'] = mem_info.used
-        info[i]['total'] = mem_info.total
-        info[i]['temperature'] = pynvml.nvmlDeviceGetTemperature(handle, pynvml.NVML_TEMPERATURE_GPU)
-        info[i]['fan_speed'] = fan_speed
+        power_usage = pynvml.nvmlDeviceGetPowerUsage(handle)
+        power_state = pynvml.nvmlDeviceGetPowerState(handle)
+        temperature = pynvml.nvmlDeviceGetTemperature(
+            handle, pynvml.NVML_TEMPERATURE_GPU
+        )
+        info.append(
+            Device(
+                free=mem_info.free,
+                used=mem_info.used,
+                total=mem_info.total,
+                temperature=temperature,
+                fan_speed=fan_speed,
+                power_usage=power_usage,
+                power_state=power_state,
+                process=process_info,
+            )
+        )
 
     infos["count"] = device_count
     infos["driver_version"] = driver_version
